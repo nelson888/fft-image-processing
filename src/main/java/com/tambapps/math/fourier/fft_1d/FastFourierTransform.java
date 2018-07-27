745fcd81bbf086710aba4392d1a67d338161a339
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2018-07-27 15:40:44 +0200

    implemented FFT1D and FFT2D

--- src/main/java/com/tambapps/math/fourier/fft_1d/FastFourierTransform.java
+++ src/main/java/com/tambapps/math/fourier/fft_1d/FastFourierTransform.java
@@ -1,4 +1,72 @@
 package com.tambapps.math.fourier.fft_1d;
 
-public class FastFourrierTransformer1D {
+import com.tambapps.math.complex.Complex;
+import com.tambapps.math.util.PowerOfTwo;
+import com.tambapps.math.util.Vector;
+
+//from https://rosettacode.org/wiki/Fast_Fourier_transform#Java
+public class FastFourierTransform {
+
+  /**
+   * Compute the 1D FFT in the given array
+   * @param array the discrete function to compute the DFT
+   */
+  public static void iterativeFFT(Vector<Complex> array) {
+    int bits = PowerOfTwo.getExponent(array.getSize());
+    bitReverseArray(array, bits);
+
+    int n = array.getSize();
+
+    for (int m = 2; m <= n ; m*=2) {
+      double dM = (double)m;
+      for (int i = 0; i < n; i++) {
+        for (int k = 0; k < m / 2; k++) {
+          int evenIndex = i + k;
+          int oddIndex = i + k + (m / 2);
+          Complex even = array.getElement(evenIndex);
+          Complex odd = array.getElement(oddIndex);
+
+          Complex wm = Complex.expI(-2 * Math.PI * k / dM).mul(odd);
+          array.setElement(evenIndex, wm.add(even));
+          array.setElement(oddIndex, odd.sub(wm));
+        }
+      }
+    }
+  }
+
+  private static int bitReversedIndex(int i, int bits) {
+    int reversedN = i;
+    int count = bits - 1;
+
+    i >>= 1;
+    while (i > 0) {
+      reversedN = (reversedN << 1) | (i & 1);
+      count--;
+      i >>= 1;
+    }
+
+    return ((reversedN << count) & ((1 << bits) - 1));
+  }
+
+  private static void bitReverseArray(Vector<Complex> buffer, int bits) {
+    for (int j = 1; j < buffer.getSize() / 2; j++) {
+      int swapPos = bitReversedIndex(j, bits);
+      Complex temp = buffer.getElement(j);
+      buffer.setElement(j, buffer.getElement(swapPos));
+      buffer.setElement(swapPos, temp);
+    }
+  }
+
+  public static void inverse(Vector<Complex> array) {
+    for (int i = 0; i < array.getSize(); i++) {
+      array.setElement(i, array.getElement(i).conj());
+    }
+
+    iterativeFFT(array);
+
+    double iN = 1d / ((double)array.getSize());
+    for (int i = 0; i < array.getSize(); i++) {
+      array.setElement(i, array.getElement(i).conj().scl(iN));
+    }
+  }
 }
--- src/main/java/com/tambapps/math/fourier/fft_2d/FastFourierTransformer2D.java
+++ src/main/java/com/tambapps/math/fourier/fft_2d/FastFourierTransformer2D.java
@@ -1,9 +1,9 @@
 package com.tambapps.math.fourier.fft_2d;
 
-import com.tambapps.math.array_2d.Array2D;
 import com.tambapps.math.array_2d.Complex2DArray;
-import com.tambapps.math.array_2d.Integer2DArray;
 import com.tambapps.math.complex.Complex;
+import com.tambapps.math.fourier.fft_1d.FastFourierTransform;
+import com.tambapps.math.util.Vector;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorCompletionService;
@@ -12,8 +12,12 @@ import java.util.concurrent.ExecutorService;
 //TODO TO IMPLEMENT This shows that a 2D FFT can be broken down into a series of 1D Fourier transforms.
 // To compute a 2D FFT, 1D Fourier transform is applied to each individual row of the input matrix and then to each column.
 //TODO faire des fonctions qui aggrandissent l'image comme dans le TP (permet une plus grande precision)
+
+
+
+//we apply the 1D fft on each row then on each column
 public class FastFourierTransformer2D {
-  private final int maxThreads;
+  private final double maxThreads;
   private final ExecutorCompletionService<Void> executorService;
 
   public FastFourierTransformer2D(ExecutorService executor, int maxThreads) {
@@ -21,134 +25,102 @@ public class FastFourierTransformer2D {
     this.maxThreads = maxThreads;
   }
 
-  public Complex2DArray transform(Integer2DArray f) {
-    int M = f.getM();
-    int N = f.getN();
-    int total = N * M;
-    int from = 0;
-    int frame = total / maxThreads;
-    int tasks = 0;
+  public void transform(Complex2DArray f) {
+    transform(f, false, true);
+    transform(f, false, false);
+  }
 
-    Complex2DArray fft = new Complex2DArray(M, N);
+  public void inverse(Complex2DArray f) {
+    transform(f, true, true);
+    transform(f, true, false);
+  }
 
-    while (from < total) {
-      executorService.submit(new TransformTask(M, N, f, fft, from, Math.min(from + frame, total)));
-      tasks++;
-      from += frame;
+  private void transform(Complex2DArray f, boolean inverse, boolean row) {
+    int treated = 0;
+    int max = row ? f.getM() : f.getN();
+    int perThread = (int) Math.floor(((double)max) / maxThreads);
+    int count = 0;
+
+    while (treated < max) {
+      if (inverse) {
+        executorService.submit(new InverseTask(f, treated, Math.min(max, treated + perThread), row));
+      } else {
+        executorService.submit(new TransformTask(f, treated, Math.min(max, treated + perThread), row));
+      }
+
+      treated += perThread;
+      count++;
     }
 
-    for (int i = 0; i < tasks; i++) {
+    for (int i = 0; i < count; i++) {
       try {
         executorService.take();
       } catch (InterruptedException e) {
-        e.printStackTrace();
-        return null;
+        throw new RuntimeException("Couldn't wait longer");
       }
     }
-
-    return fft;
   }
 
-
-  /**
-   * Task that will do some computation between 'from' and 'to'
-   * ('from' and 'to' represent the 2D array indexes in 1D)
-   */
-  private abstract class FourierTask<T> implements Callable<Void> {
-    private final int M;
-    private final int N;
-    private final Array2D<T> input;
-    private final Complex2DArray output;
+  private abstract class FourierTask implements Callable<Void> {
+    private final Complex2DArray data;
     private final int from;
     private final int to;
+    private final boolean row;
 
-    /**
-     * @param input the input of the task
-     * @param output the result of the task
-     * @param from the starting index in 1D
-     * @param to the ending index in 1D (excluded)
-     */
-    FourierTask(int m, int n, Array2D<T> input, Complex2DArray output, int from, int to) {
-      M = m;
-      N = n;
-      this.input = input;
-      this.output = output;
+
+    FourierTask(Complex2DArray data, int from, int to, boolean row) {
+      this.data = data;
       this.from = from;
       this.to = to;
+      this.row = row;
     }
 
     @Override
     public final Void call() {
-      final double dM = (double)M;
-      final double dN = (double)N;
+      if (row) {
+        for (int i = from; i < to; i++) {
+          computeVector(data.getRow(i));
 
-      //transforming the 1D in 2D
-      int i = from % M;
-      int j = from / M;
-
-      while (j * M + i < to) {
-        output.set(i, j, computePoint(dM, dN, input, i, j));
-        if (i == M - 1) {
-          i = 0;
-          j++;
-        } else {
-          i++;
+        }
+      } else {
+        for (int i = from; i < to; i++) {
+          computeVector(data.getColumn(i));
         }
       }
       return null;
     }
 
-    abstract Complex computePoint(double M, double N, Array2D<T> f, int m, int n);
+    abstract void computeVector(Vector<Complex> vector);
   }
 
   /**
-   * Task that will compute the FFT  between 'from' and 'to'
+   * Task that will compute the FFT for many columns/rows
    */
-  private class TransformTask extends FourierTask<Integer> {
+  private class TransformTask extends FourierTask {
 
-    /**
-     * @param fft the result fft
-     * @param f the function to transform
-     * @param from the starting index in 1D
-     * @param to the ending index in 1D (excluded)
-     */
-    TransformTask(int M, int N, Integer2DArray f, Complex2DArray fft, int from, int to) {
-      super(M, N, f, fft, from, to);
+    TransformTask(Complex2DArray data, int from, int to, boolean row) {
+      super(data, from, to, row);
     }
 
     @Override
-    Complex computePoint(double M, double N, Array2D<Integer> f, int m, int n) {
-      Complex result = Complex.ZERO;
-      for (int i = 0; i < f.getM(); i++) {
-        for (int j=0; j < f.getN(); j++) {
-          result = result.add(Complex.expI(-2 * Math.PI * (i*m/M + j*n/N)).scl(f.get(i,j))); //TODO to simply (to much complex creation)
-        }
-      }
-
-      return result;
+    void computeVector(Vector<Complex> vector) {
+      FastFourierTransform.iterativeFFT(vector);
     }
+
   }
 
   /**
-   * Task that will compute the inverse of the given FFT  between 'from' and 'to'
+   * Task that will compute the inverse FFT for many columns/rows
    */
-  private class InverseTask extends FourierTask<Complex> {
+  private class InverseTask extends FourierTask {
 
-    InverseTask(int M, int N, Complex2DArray fft, Complex2DArray f, int from, int to) {
-      super(M, N, fft, f, from, to);
+    InverseTask(Complex2DArray data, int from, int to, boolean row) {
+      super(data, from, to, row);
     }
 
-    @Override
-    Complex computePoint(double M, double N, Array2D<Complex> fft, int m, int n) {
-      Complex result = Complex.ZERO;
-      for (int i = 0; i < fft.getM(); i++) {
-        for (int j=0; j < fft.getN(); j++) {
-          result = result.add(Complex.expI(2 * Math.PI * (i*m/M + j*n/N)).mul(fft.get(i, j)));
-        }
-      }
-
-      return result.scl(1d / (M * N));
+    @Override void computeVector(Vector<Complex> vector) {
+      FastFourierTransform.inverse(vector);
     }
+
   }
-
 }
