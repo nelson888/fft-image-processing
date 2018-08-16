5cc9893d2fa13f77477dc77d8d3071117d7752d7
Nelson <tambue@hotmail.fr>
2018-08-16 11:57:53 +0200

    corrected bug in padding function

--- src/main/java/com/tambapps/math/fourier/util/FFTUtils.java
+++ src/main/java/com/tambapps/math/fourier/util/FFTUtils.java
@@ -51,8 +51,8 @@ public class FFTUtils {
         array.getN() + paddingLeft + paddingRight);
     for (int i = 0; i < copy.getM(); i++) {
       for (int j = 0; j < copy.getN(); j++) {
-        if (i < paddingEnd || i >= paddingTop + array.getM() ||
-            j < paddingLeft || j >= paddingRight + array.getN()) {
+        if (i < paddingEnd || i >= array.getM() - paddingTop ||
+            j < paddingLeft || j >= array.getN() - paddingRight) {
           copy.set(i, j, Complex.ZERO);
         } else {
           copy.set(i, j, array.get(i - paddingEnd, j - paddingLeft));
